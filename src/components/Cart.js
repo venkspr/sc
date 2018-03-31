9dcf7f0f1634297f5c49d25f29be6081ca747162
Samer Buna <samer.buna@gmail.com>
2018-03-30 17:15:42 -0700

    total

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 class Cart extends React.PureComponent {
+  cartTotal = () =>
+    this.props.products
+      .reduce((acc, curr) => acc + curr.Price, 0) / 100;
+
   render() {
     return (
-      <ul>
-        {this.props.products.map((product) => (
-          <li key={product.id}>
-            {product.CrossReference} - ${product.Price / 100}
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <ul>
+          {this.props.products.map((product) => (
+            <li key={product.id}>
+              {product.CrossReference} - ${product.Price / 100}
+            </li>
+          ))}
+        </ul>
+        <div>Total: ${this.cartTotal()}</div>
+      </React.Fragment>
     );
   }
 }
